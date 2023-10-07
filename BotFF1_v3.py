dc5549c35c5eefeae8e81a5f80e04b1f26a8749b
FamsFamily <145118246+FamsFamily@users.noreply.github.com>
2023-10-07 10:55:20 +0700

    Update BotFF1_v3.py

--- BotFF1_v3.py
+++ BotFF1_v3.py
@@ -15,7 +15,35 @@ async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def he(ctx, count_he = 3):
+    await ctx.send("he" * count_he)
+
+@bot.command()
+async def wk(ctx, count_wk = 3):
+    await ctx.send("wk" * count_wk)
+
+@bot.command()
+async def hi(ctx, count_hi = 3):
+    await ctx.send("hi" * count_hi)
+
+@bot.command()
+async def ha(ctx, count_ha = 3):
+    await ctx.send("ha" * count_ha)
+
+@bot.command()
+async def hiks(ctx, count_hiks = 3):
+    await ctx.send("hiks " * count_hiks)
+
+@bot.command()
+async def ho(ctx, count_ho = 3):
+    await ctx.send("ho" * count_ho)
+
+@bot.command()
+async def hu(ctx, count_hu = 3):
+    await ctx.send("hu" * count_hu)
+
+@bot.command()
+async def uhuk(ctx, count_uhuk = 3):
+    await ctx.send("uhuk " * count_uhuk)
 
 bot.run("TOKEN RAHASIA ADA DI SINI")
